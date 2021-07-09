797a74a9de3b00406987003d1c38fb27da40c2ae
fluxfw <fluxfw@fluxlabs.ch>
2021-07-09 09:06:25 +0200

    Remove ILIAS 5.4 support

--- src/Component/Table.php
+++ src/Component/Table.php
@@ -4,7 +4,6 @@ namespace srag\DataTableUI\Component;
 
 use ILIAS\UI\Component\Component;
 use ILIAS\UI\Component\Input\Field\FilterInput;
-use ILIAS\UI\Component\Input\Field\Input as FilterInput54;
 use srag\DataTableUI\Component\Column\Column;
 use srag\DataTableUI\Component\Data\Fetcher\DataFetcher;
 use srag\DataTableUI\Component\Format\Browser\BrowserFormat;
@@ -60,7 +59,7 @@ interface Table extends Component, Pluginable
 
 
     /**
-     * @return FilterInput[]|FilterInput54[]
+     * @return FilterInput[]
      */
     public function getFilterFields() : array;
 
@@ -128,7 +127,7 @@ interface Table extends Component, Pluginable
 
 
     /**
-     * @param FilterInput[]|FilterInput54[] $filter_fields
+     * @param FilterInput[] $filter_fields
      *
      * @return self
      */
